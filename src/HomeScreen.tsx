bc7b3e1d8a439228b6c3e43d51b558d4e4ab492f
Quek Ruo Ling <ruoling@Queks-MacBook-Pro.local>
2021-02-03 15:51:57 +0800

    Add Typescript config

--- src/HomeScreen.tsx
+++ src/HomeScreen.tsx
@@ -28,6 +28,7 @@ const HomeScreen = ({ navigation, route }) => {
           })
         }
       />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
       <Button
         title="Update the title"
         onPress={() => navigation.setOptions({ title: 'Updated!' })}
