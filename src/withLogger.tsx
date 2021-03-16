c89053111dabfa125588f671166fc76d9bc0b0e0
Roll <quek.ruoling@gmail.com>
2021-03-16 19:51:57 +0800

    Log on focus

--- src/withLogger.tsx
+++ src/withLogger.tsx
@@ -1,11 +1,21 @@
 import { useFocusEffect } from '@react-navigation/native';
 import * as React from 'react';
-import { NavigationRef } from './NavigationRef';
 
 export const withLogger = (Component, options?) => (props) => {
-  console.log(
-    `@@@ ${Component.name} => ${JSON.stringify(props.route.params, null, 2)}`,
-  );
+  // console.log(
+  //   `@@@ ${Component.name} => ${JSON.stringify(props.route.params, null, 2)}`,
+  // );
+
+  React.useEffect(() => {
+    console.log(`@@@ ${Component.name} mounted`);
+    return () => console.log(`@@@ ${Component.name} unmounted`);
+  }, []);
+
+  const onFocus = React.useCallback(() => {
+    console.log(`@@@ ${Component.name} focused`);
+  }, []);
+
+  useFocusEffect(onFocus);
 
   return <Component {...props} />;
 };
